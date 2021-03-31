1f038ff703884b287668e990c3485955281531e6
EVerrycken <evert.verrycken@student.ucll.be>
2021-03-31 15:32:05 +0200

    Updated data

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,3 +1,14 @@
+--insert into hellohopper.subscriptions (id,title,description,thumbnail_url,quantity,price) values
+--('1','','This is a test subscription','',1,0),
+--('2','','This is a test subscription','',1,0);
+
+
 insert into hellohopper.subscriptions (id,title,description,thumbnail_url,quantity,price) values
-('1','','This is a test subscription','',1,0),
-('2','','This is a test subscription','',1,0);
+('1','Antamole','Black ant guacamole','',10,7.50),
+('2','Carrot buffalo worm soup','Placeholder','',10,6.00),
+('3','Cricket, mealworm and grasshopper burger','Placeholder','',5,10.50),
+('4','Banana bread with cricket powder','Placeholder','',20,5.50),
+('5','Maroccan summer salad','Placeholder','',30,6.00),
+('6','Paleo cricket snaps','Placeholder','',50,4.00),
+('7','Japanese ramen nagi','Placeholder','',20,7.00),
+('8','Mexican Hopper','Queso de cincho with fried grasshoppers','',10,12.00);
