59acfa3cd33727230b710cfefac940d8e29828e3
EVerrycken <evert.verrycken@student.ucll.be>
2021-04-01 09:52:56 +0200

    Updated data

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,25 +1,25 @@
 insert into hellohopper.subscriptions (id,title,description,thumbnail_url,quantity,price) values
-('1','1 small Bug Box / month','Food for 2 people per box','',1,10.00),
-('2','2 small Bug Boxes / month','Food for 2 people per box','',1,20.00),
-('3','3 small Bug Boxes / month','Food for 2 people per box','',1,30.00),
-('4','4 small Bug Boxes / month','Food for 2 people per box','',1,40.00),
-('5','1 medium Bug Box / month','Food for 3 people per box','',1,13.00),
-('6','2 medium Bug Boxes / month','Food for 3 people per box','',1,26.00),
-('7','3 medium Bug Boxes / month','Food for 3 people per box','',1,39.00),
-('8','4 medium Bug Boxes / month','Food for 3 people per box','',1,52.00),
-('9','1 large Bug Box / month','Food for 4 people per box','',1,15.00),
-('10','2 large Bug Boxes / month','Food for 4 people per box','',1,30.00),
-('11','3 large Bug Boxes / month','Food for 4 people per box','',1,45.00),
-('12','4 large Bug Boxes / month','Food for 4 people per box','',1,60.00);
+('1','1 small Hopper Box / month','Food for 2 people per box','small.png',1,10.00),
+('2','2 small Hopper Boxes / month','Food for 2 people per box','small.png',1,20.00),
+('3','3 small Hopper Boxes / month','Food for 2 people per box','small.png',1,30.00),
+('4','4 small Hopper Boxes / month','Food for 2 people per box','small.png',1,40.00),
+('5','1 medium Hopper Box / month','Food for 3 people per box','medium.png',1,13.00),
+('6','2 medium Hopper Boxes / month','Food for 3 people per box','medium.png',1,26.00),
+('7','3 medium Hopper Boxes / month','Food for 3 people per box','medium.png',1,39.00),
+('8','4 medium Hopper Boxes / month','Food for 3 people per box','medium.png',1,52.00),
+('9','1 large Hopper Box / month','Food for 4 people per box','large.png',1,15.00),
+('10','2 large Hopper Boxes / month','Food for 4 people per box','large.png',1,30.00),
+('11','3 large Hopper Boxes / month','Food for 4 people per box','large.png',1,45.00),
+('12','4 large Hopper Boxes / month','Food for 4 people per box','large.png',1,60.00);
 
 
 
---insert into hellohopper.subscriptions (id,title,description,quantity,price) values
---('1','Small bug box - 2 persons','Black ant guacamole','black_ant_quacamole.jpg',10,7.50),
---('2','','Carrot buffalo worm soup','carrot_buffalo_worm_soop.jpg',10,6.00),
---('3','','Cricket, mealworm and grasshopper burger','cricket_mealworm_grasshopper_burger.png',5,10.50),
---('4','','Banana bread with cricket powder','cricket_powder_bananabread.jpg',20,5.50),
---('5','','Moroccan summer salad','maroccan_summer_salad.jpg',30,6.00),
---('6','','Paleo cricket snaps','paleo_cricket_snaps.jpg',50,4.00),
---('7','','Japanese ramen nagi','ramen_nagi.jpg',20,7.00),
---('8','','Mexican queso de cincho with fried grasshoppers','big_mexican_grasshoppers.png',10,12.00);
+insert into hellohopper.recipes (id,title,thumbnail_url) values
+('1','Black ant guacamole','black_ant_quacamole.jpg'),
+('2','Carrot buffalo worm soup','carrot_buffalo_worm_soop.jpg'),
+('3','Cricket, mealworm and grasshopper burger','cricket_mealworm_grasshopper_burger.png'),
+('4','Banana bread with cricket powder','cricket_powder_bananabread.jpg'),
+('5','Moroccan summer salad','maroccan_summer_salad.jpg'),
+('6','Paleo cricket snaps','paleo_cricket_snaps.jpg'),
+('7','Japanese ramen nagi','ramen_nagi.jpg'),
+('8','Mexican queso de cincho with fried grasshoppers','big_mexican_grasshoppers.png');
--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,16 +1,16 @@
 drop table if exists hellohopper.subscriptions;
 create table if not exists hellohopper.subscriptions (
-	id varchar(255) primary key,
-	title varchar(255),
+    id varchar(255) primary key,
+    title varchar(255),
     description varchar(255),
     thumbnail_url varchar(255),
     quantity int,
     price decimal(10,2)
 );
 
---drop table if exists hellohopper.recipes;
---create table if not exists hellohopper.recipes (
---    id bigint,
---    title varchar(255) primary key,
---    thumbnail_url varchar(255)
---);
+drop table if exists hellohopper.recipes;
+create table if not exists hellohopper.recipes (
+    id int,
+    title varchar(255) primary key,
+    thumbnail_url varchar(255)
+);
